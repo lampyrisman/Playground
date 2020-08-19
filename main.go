71310c5f6b59a667bdf26164a1d8c229482624ff
trurl <trurl@desk.vol>
2020-08-19 14:39:08 +0300

    check hook

--- main.go
+++ main.go
@@ -11,6 +11,18 @@ import (
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
     var config *pgconn.Config
+    config.Host = "pg.sm"
+    config.Port = "5432"
+    config.Database = "spaceworld"
+    config.User = "site"
+    config.Password = "siteread"
+
+    conn, err := pgx.Connect(*config)
+    if err != nil {
+    panic(err)
+    }
+    defer conn.Close()
+
 
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
