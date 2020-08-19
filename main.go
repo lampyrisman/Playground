89379e77aa08c37aa7c6b6b4f98687936032c0f3
trurl <trurl@desk.vol>
2020-08-19 14:26:47 +0300

    check hook

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-    var config Config
+    var config *pgx.Config
 
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
