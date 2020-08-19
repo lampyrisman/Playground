3d9c3c15d89acfc5e90a4369982b462fc4380237
trurl <trurl@desk.vol>
2020-08-19 14:39:48 +0300

    check hook

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
     config.User = "site"
     config.Password = "siteread"
 
-    conn, err := pgx.Connect(*config)
+    conn, err := pgx.ConnectConfig(*config)
     if err != nil {
     panic(err)
     }
