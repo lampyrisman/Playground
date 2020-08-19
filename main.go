1ff23764bbc8c4908b38c578eda255b98adc3e0c
trurl <trurl@desk.vol>
2020-08-19 18:15:16 +0300

    check hook

--- main.go
+++ main.go
@@ -30,21 +30,21 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
     conn, err := pgx.ConnectConfig(context.Background(), pgconfig)
     if err != nil {
-    panic(err)
+    fmt.Println(err)
     }
     defer conn.Close(context.Background())
 
     rows, err := conn.Query(context.Background(), "select id,level,parent,fieldname,fieldtype,fieldorder from catalog.menu")
     if err != nil {
-	panic(err)
+	fmt.Println(err)
     }
 
-    for rows.Next() {
-	var menuItem MenuStruct
-	err = rows.Scan(&menuItem.Id, &menuItem.Level, &menuItem.Parent, &menuItem.Fieldname, &menuItem.Fieldtype, &menuItem.Fieldorder)
-	if err != nil {
-	    panic(err)
-	}
+//    for rows.Next() {
+//	var menuItem MenuStruct
+//	err = rows.Scan(&menuItem.Id, &menuItem.Level, &menuItem.Parent, &menuItem.Fieldname, &menuItem.Fieldtype, &menuItem.Fieldorder)
+//	if err != nil {
+//	    panic(err)
+//	}
 //	menuItems = menuItems +" | "+  menuItem.Fieldname
     }
     defer rows.Close()
