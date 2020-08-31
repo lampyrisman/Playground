ceb40ee4ee40e7793337f743fcebc1c154cb4baf
trurl <trurl@desk.vol>
2020-08-31 13:07:29 +0300

    check hook

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
     defer conn.Close(context.Background())
     err = conn.QueryRow(context.Background(), "select max(level) from catalog.mainmenu;").Scan(&maxlevel)
 
-    var inputArray = make([][]MenuStruct)
+    var inputArray = make([][]MenuStruct, maxlevel)
         
     rows, err := conn.Query(context.Background(), "select id,level,parent,fieldorder,fieldname from catalog.mainmenu order by level")
     if err != nil {
