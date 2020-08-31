e1573444ee0d2b9366693491ca256805c7913b39
trurl <trurl@desk.vol>
2020-08-31 13:54:15 +0300

    check hook

--- main.go
+++ main.go
@@ -54,14 +54,22 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
     fmt.Println("inputArray len (menu levels) = ",len(inputArray))
     
-    levelUp := make(map[int]string)
+    levelUp, levelDown := make(map[int]string)
 
-    for i := len(inputArray)-1; i >= 1; i-- {
-	fmt.Println("level", i, "content", inputArray[i])
-	for _,inVal := range inputArray[i]{
-	    levelUp[inVal.Parent] = levelUp[inVal.Parent] + "|" + inVal.Fieldname
-	}
+    forArrLen := len(inputArray)-1
+    for i := forArrLen; i >= 1; i-- {
+	if (i == forArrLen){
+	    fmt.Println("First Element, content", inputArray[i])
+	    for _,inVal := range inputArray[i]{
+		levelUp[inVal.Parent] = levelUp[inVal.Parent] + "|" + inVal.Fieldname
+	    }
+	} else {
+	    for _,inVal := range inputArray[i]{
+		levelDown[inVal.Parent] = inVal.Fieldname + "\n\t" + levelUp[inVal.Parent]
+	    }
+	{
 	fmt.Println("Level Upper = \n",levelUp,"\n------------")
+	fmt.Println("Level Down = \n",levelDown,"\n------------")
     }
 
     tMenu := template.New("menu")
